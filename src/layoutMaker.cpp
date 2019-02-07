f8f2d4fe21cae6eba3b2d4a2b0d57782a2ef6da9
Amit Segall <Amitsegall@gmail.com>
2019-02-07 20:07:02 +0000

    changed layoutmaker
    now I can find bigger shapes in layout and also have different settings of expression according to size.

--- src/layoutMaker.cpp
+++ src/layoutMaker.cpp
@@ -89,6 +89,8 @@ void LayoutMaker::findShapesInImage(ofImage myImage){
     
     // find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
     // also, find holes is set to true so we will get interior contours as well....
-    contourFinder.findContours(grayDiff, 20, (500*240)/3, 100, false);// find holes
+    contourFinder.findContours(grayDiff, 20, (1280*240)/3, 100, false);// find holes
+    
+    
     
 }
